bc1c807a352b6b0d9e905ca53981e9ebf9d87d85
Russell <rnc720@gmail.com>
2023-07-31 20:38:15 -0500

    Added logic and content to display projects

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,14 @@
 // Import Required
 import React from 'react';
 import About from './Pages/About';
+import Project from './Pages/Project';
 
 // Content component
 function Content () {
     return (
         <div className='container-fluid text-center content'>
             <About />
+            <Project />
 
         </div>
     )
